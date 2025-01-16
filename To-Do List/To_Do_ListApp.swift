831b5734650149afcf189b29917968472a3a2c7d
tyler0611 <berlity0611@gmail.com>
2025-01-16 14:11:20 -0600

    Initial Commit

---
+++ To-Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do List
+//
+//  Created by Tyler Berlin on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
